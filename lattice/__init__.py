a13c774877bef17e9bb5adc03ac71274c4baeebf
ljvmiranda921 <lester.miranda@obf.ateneo.edu>
2018-04-03 20:04:08 +0900

    Initial commit
    Signed-off-by: ljvmiranda921 <lester.miranda@obf.ateneo.edu>

---
+++ lattice/__init__.py
@@ -0,0 +1,9 @@
+"""
+Lattice
+=======
+Lattice is a Python-library that provides a 2D HP Lattice environment in a 
+Gym-like environment
+"""
+
+__author__ = """Lester James V. Miranda"""
+__email__ = 'ljvmiranda@gmail.com'
