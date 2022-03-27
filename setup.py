42c9d87c48bb06adf77698d6ffe4b861ffe10494
Mohammed Eltahir <mseeltahir@gmail.com>
2022-03-27 23:32:24 +0200

    updated the versions of package
    The previous versions don't seem to work with newer versions of Python, so
    the library wasn't working for me. When I installed the packages by hand
    and ran the tests it worked fine. So I just updated the packages in
    requirements.txt and setup.py.

--- setup.py
+++ setup.py
@@ -1,21 +1,16 @@
 from setuptools import setup
 
-requirements = [
-      'gym==0.10.4',
-      'numpy==1.14.2'
-]
+requirements = ["gym==0.23.1", "numpy==1.22.3"]
 
-dev_requirements = [
-      'pytest==3.2.1',
-      'tox==3.0.0'
-]
+dev_requirements = ["pytest==7.1.1", "tox==3.24.5"]
 
-setup(name='gym_lattice',
-      version='0.0.2',
-      install_requires=requirements,
-      description="An HP 2D Lattice Gym Environment for Protein Folding",
-      author="Lester James V. Miranda",
-      author_email='ljvmiranda@gmail.com',
-      url='https://github.com/ljvmiranda921/gym-lattice',
-      test_require=dev_requirements
+setup(
+    name="gym_lattice",
+    version="0.0.2",
+    install_requires=requirements,
+    description="An HP 2D Lattice Gym Environment for Protein Folding",
+    author="Lester James V. Miranda",
+    author_email="ljvmiranda@gmail.com",
+    url="https://github.com/ljvmiranda921/gym-lattice",
+    test_require=dev_requirements,
 )
